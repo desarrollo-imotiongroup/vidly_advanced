da1ddd7c6601acd6e0e25ad6fc94e0e918481ea6
nasirmehmood <nasirmalik9641100@gmail.com>
2025-05-05 22:09:40 +0200

    Setting env

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => winston.info('Connecting to PORT:', PORT));
